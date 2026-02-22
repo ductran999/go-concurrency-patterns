00240b2e7bb5c2bc6edba0789559643b55710c85
Daniel <74949792+ductran999@users.noreply.github.com>
2026-02-22 11:40:41 +0700

    apply singleflight

--- case_study/gogw/main.go
+++ case_study/gogw/main.go
@@ -1,4 +1,72 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"strings"
+	"time"
+
+	"golang.org/x/sync/singleflight"
+)
+
+var (
+	ErrInvalidToken = errors.New("invalid token")
+)
+
+type ReverseProxy struct {
+	authGroup singleflight.Group
+}
+
+func (a *ReverseProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	token := strings.TrimSpace(authHeader)
+
+	if token == "" {
+		http.Error(w, "Missing Authorization Header", http.StatusUnauthorized)
+		return
+	}
+
+	v, err, shared := a.authGroup.Do(token, func() (any, error) {
+		return a.validateTokenWithAuthService(token)
+	})
+
+	if err != nil {
+		http.Error(w, "Auth Error", http.StatusInternalServerError)
+		return
+	}
+
+	isValid, ok := v.(bool)
+	if !ok {
+		http.Error(w, "invalid data type", http.StatusInternalServerError)
+		return
+	}
+	if shared {
+		fmt.Printf("Token [%s]: reuse result!\n", token)
+	}
+
+	if !isValid {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	_, _ = w.Write([]byte("OK - Forwarding to Model"))
+}
+
+func (a *ReverseProxy) validateTokenWithAuthService(token string) (bool, error) {
+	slog.Info("Call Auth Service...")
+	time.Sleep(500 * time.Millisecond)
+	if !strings.HasPrefix(token, "Bearer") {
+		return false, ErrInvalidToken
+	}
+
+	return true, nil
+}
+
 func main() {
+	ReverseProxy := &ReverseProxy{}
+	if err := http.ListenAndServe(":8080", ReverseProxy); err != nil && errors.Is(err, http.ErrServerClosed) {
+		slog.Error(err.Error())
+	}
 }
--- go.mod
+++ go.mod
@@ -4,13 +4,14 @@ go 1.24.0
 
 require (
 	github.com/DucTran999/shared-pkg v0.0.0-20250701065240-034489fcce32
+	github.com/james-bowman/sparse v0.0.0-20260216202247-495ee4f84d35
 	github.com/stretchr/testify v1.10.0
+	golang.org/x/sync v0.13.0
 	gonum.org/v1/gonum v0.17.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/james-bowman/sparse v0.0.0-20260216202247-495ee4f84d35 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
